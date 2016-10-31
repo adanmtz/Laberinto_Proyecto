ea3ba0780ebce8baa919a3257475ff0134f16b34
adanmtz <ingmtz03@hotmail.com>
2016-10-30 18:53:41 -0600

    Update proyecto.cpp

--- proyecto.cpp
+++ proyecto.cpp
@@ -1,84 +1,88 @@
-/*Amigos, podemos usar este código y ya solo lo modificamos para las caracteristicas qu pide el profe, está en C, y yá es solo modificar
-y/o agregar variables, funciones, etc., cualquier cosa por correo o whats, avisenme si están de acuerdo para empezar sobre este código
-ma.millan@live.com*/
-
+/*Modifiqué la lectura del archivo para que sea leído como parámetro en main, así como leer el laberinto por memoria dinámica, es decir,
+en las funciones de leerArchivo, dimensionMatriz y algo del main */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <conio.h>
 #include <stdbool.h>
- 
-int m[100][100]; // Matriz que guarda los datos del archivo. El maximo del laberinto son 100 filas por 100 columnas. 
-                 //Se puede modificar
- 
-int f,c; // filas y columnas del laberinto
+
+void leerArchivo();
+void MuestraLaberinto();
+void mueveJugador(int);
+void dimensionaMatriz();
+void RecorrerLaberinto() {
+
+int **mat; // Matriz dinamica que guarda los datos del archivo.            
+//int m2[100][100];   // Esta matriz mostrara la solucion, aun no la uso
+int i, j;
+int fil,col; // filas y columnas del laberinto            
 int fp,cp; // coordenadas del jugador
-int fs,cs; // coordenadas de la salida o meta
+int xfin,yfin; // coordenadas de la salida o meta
+int xini,yini; // coordenadas iniciales del jugador
 const int ARRIBA=0, DERECHA=1, IZQUIERDA=2, ABAJO=3;
 int direccion=0; // El jugador inicia viendo hacia arriba
+int movs; // Cuenta los movimientos que hace un jugador para llegar a la meta
+int espacios; // Cuenta los espacios que hay en un laberitno
+bool ya=false; // Indica si ya pasamos por ese lugar
 int arriba, abajo, izquierda, derecha; // Aqui guardamos la informacion de la matriz de las 
                                              // cuatro direcciones junto al jugador
- 
-void leerArchivo() {
-     int maxCol;
+
+void dimensionaMatriz(char ruta[]) {
+     bool flag=true; // Sirve para que aumente el numero de columnas solo hasta que encuentre el primer salto de linea
+                     // suponiendo que el archivo tiene todas las lineas de igual numero de caracteres
     FILE *archivo;
     char caracter;
     int codigo;
-    bool flag=true; // Sirve para que aumente el numero de columnas solo hasta que encuentre le primer salto de linea
-               // suponiendo que el archivo tiene todas las lineas de igual numero de caracteres
- 
-        archivo=fopen("laberinto.txt","rt"); // abrimos el archivo "laberinto.txt" en modo lectura de texto("rt"
-        if (archivo==NULL) { // En caso de que el archivo no exista o que este mal el nombre
-           printf("Error en lectura de archivo\n");
-           system("pause");
-           exit (EXIT_FAILURE); // Terminacion del programa por falla
-           }
- 
-        while(!feof(archivo)) { // Este ciclo sirve para dimensionar nuestra matriz
-                caracter=fgetc(archivo);
-                codigo = caracter; // obtenemos el codigo Ascii de el caracter
- 
-                if (codigo==10) { // codigo del salto de linea
-                   f++; // Si encontramos un enter añadimos una fila mas a nuestra matriz
-                   if (flag) {maxCol=c; flag=false;} // solo lo hacemos una vez
-                   c=0;
-                } else {
-                       m[f][c]=codigo-48;
-                       if (m[f][c]==2){fp=f;cp=c;}
-                       if (m[f][c]==3){fs=f;cs=c;}
-                       c++ ;// añadimos una columna mas a nuestra matriz
-                }
-        }
-        c=maxCol; // si no ponemos esto la matriz queda de una columna
-        fclose(archivo);
-        //printf ("%d filas, %d columnas",f,maxCol); // para debug
-}
-/* 
-void MuestraLaberinto() {
-     // En esta funcion las paredes se muestran como asteriscos "*", el jugador como "J" y la meta como "M"
-    system("cls");
-    for (int i=0;i<f;i++) {
-        for (int ii=0;ii<c;ii++) {
-           if (m[i][ii]==1) printf ("*");
-           else if (i==fs && ii==cs) printf ("M");
-           else if (i==fp && ii==cp) printf ("J");
-           else printf (" ");
-        }
-    printf ("\n");
+    
+    archivo=fopen(ruta,"rt"); // abrimos el archivo en modo lectura de texto("rt")
+    if (archivo==NULL) { // En caso de que el archivo no exista o que este mal el nombre
+       printf("Error en lectura de archivo\n");
+       system("pause");
+       exit (EXIT_FAILURE); // Terminacion del programa por falla
     }
-    //printf ("Direccion %d, arriba %d, abajo %d, derecha %d, izquierda %d\n",direccion,arriba,abajo,derecha,izquierda); // debug
-    system("pause"); // esperamos que el usuario presione una tecla
+    fil=1;col=1;
+    while(!feof(archivo)) { // Dimensionamos nuestra matriz
+            caracter=fgetc(archivo);
+            codigo = caracter; // obtenemos el codigo Ascii de el caracter
+            
+            if (codigo==10) { // codigo del salto de linea
+               fil++; // Si encontramos un enter añadimos una fila mas a nuestra matriz
+               if (flag) flag=false; // solo lo hacemos una vez
+            } else if (flag) col++;// añadimos una columna mas a nuestra matriz               
+    }
+    fclose(archivo); 
+    
+    // Dimensionamos m[][] dinamicamente 
+    mat = (int **)malloc (fil*sizeof(int *));        
+    for (j=0;j<fil;j++) 
+    mat[j] = (int *) malloc (col*sizeof(int));      
 }
- */
+
+void leerArchivo(char ruta[]) {
+    int ff,cc,codigo;
+    FILE *archivo;
+    char caracter;      
+    
+    archivo=fopen(ruta,"rt"); // abrimos el archivo "laberinto.txt" en modo lectura de texto("rt"
+    if (archivo==NULL) { // En caso de que el archivo no exista o que este mal el nombre
+       printf("Error en lectura de archivo\n");
+       system("pause");
+       exit (EXIT_FAILURE); // Terminacion del programa por falla
+    }
+    
+    fclose(archivo);      
+}
+
 void RecorrerLaberinto() {
     int old_fp,old_cp; // Las usaremos para poner un espacio en donde estaba el jugador
  
       old_fp=fp; old_cp=cp; // Guardamos las coordenadas del jugador, para colocar un espacio cuando se mueva                   
  
-      arriba=m[fp-1][cp];
-      abajo=m[fp+1][cp];
-      derecha=m[fp][cp+1];
-      izquierda=m[fp][cp-1];
+      arriba=mat[fp-1][cp];
+      abajo=mat[fp+1][cp];
+      derecha=mat[fp][cp+1];
+      izquierda=mat[fp][cp-1];
  
       // checamos si la meta esta junto al jugador
       if (derecha==3) direccion=DERECHA;
@@ -112,32 +116,40 @@ void RecorrerLaberinto() {
          else direccion=DERECHA;// ultima opcion volver atras, giramos 
       }
  
-      m[old_fp][old_cp]=0; // ponemos un espacio donde estaba el jugador
+      mat[old_fp][old_cp]=0; // ponemos un espacio donde estaba el jugador
  
       switch(direccion){ // Movemos al jugador
         case 0:
-             m[fp-1][cp]=2; // actualizamos matriz            
+             mat[fp-1][cp]=2; // actualizamos matriz            
             fp--; // Actualizamos coordenadas del jugador         
         break;
         case 1:
-             m[fp][cp+1]=2; // actualizamos matriz            
+             mat[fp][cp+1]=2; // actualizamos matriz            
              cp++; // Actualizamos coordenadas del jugador           
         break;
         case 2:
-             m[fp][cp-1]=2; // actualizamos matriz            
+             mat[fp][cp-1]=2; // actualizamos matriz            
             cp--; // Actualizamos coordenadas del jugador           
         break;
         case 3:
-             m[fp+1][cp]=2; // actualizamos matriz           
+             mat[fp+1][cp]=2; // actualizamos matriz           
             fp++; // Actualizamos coordenadas del jugador           
         break;
       }
 }
  
 int main() {
-    leerArchivo();
+	
+	char arch[]=  "laberinto.txt"; //Nombre del archivo que tiene la informacion del laberinto
+	
+	dimensionaMatriz(arch);       
+    leerArchivo(arch); // Leemos archivo de texto con la informacion del laberinto    
+
+    movs=0; fp=xini;cp=yini;// xini y yini son los valores iniciales de las coordenadas del jugador no  cambian
+               // fp Y cp, si van cambiando en el transcurso del programa
+	
     //MuestraLaberinto();
-    while (fp!=fs || cp!=cs) {// Cuando encontramos la meta, salimos del ciclo
+    while (fp!=xfin || cp!=yfin) {// Cuando encontramos la meta, salimos del ciclo
       RecorrerLaberinto();
       //MuestraLaberinto();
     }
